31120d04c12165d274419d2503121d18a723e937
Jack Schneble <schneble.jack@gmail.com>
2022-08-28 17:33:20 -0400

    pushing

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV === 'production'){
 
 
 // Listen Server
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
